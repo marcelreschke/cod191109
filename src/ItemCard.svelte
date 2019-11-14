8586bad9fe993b9096ba3114bd29a2cc8294efa2
Marcel Reschke <hello@marcelreschke.com>
2019-11-14 14:34:45 +0100

    Entfernte Überschrift und reduziere auf ein section ItemCard in der Komponente

--- src/ItemCard.svelte
+++ src/ItemCard.svelte
@@ -2,76 +2,10 @@
 @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
 h1,p {font-family:Open Sans;}
 </style>
-<h1 style="color:navy">Unsere Studenten</h1>
 <section class="ItemCard">
     <img width="200px"src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80">
     <section> 
         <b style="font-size:25px; color:navy">Student 1</b>
-        <ul>
-        <li style="font-size:20px">Name:</li>
-        <li style="font-size:20px">Geburtsdatum:</li>
-        <li style="font-size:20px">Geschlecht:</li>
-        <li style="font-size:20px">Studiengang:</li>
-        <li style="font-size:20px">Matrikelnummer:</li>
-        </ul>
-    </section>
-</section>
-<section class="ItemCard">
-    <img width="200px"src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80">
-    <section> 
-        <b style="font-size:25px; color:navy">Student 2</b>
-        <ul>
-        <li style="font-size:20px">Name:</li>
-        <li style="font-size:20px">Geburtsdatum:</li>
-        <li style="font-size:20px">Geschlecht:</li>
-        <li style="font-size:20px">Studiengang:</li>
-        <li style="font-size:20px">Matrikelnummer:</li>
-        </ul>
-    </section>
-</section>
-<section class="ItemCard">
-    <img width="200px"src="https://images.unsplash.com/photo-1484517186945-df8151a1a871?ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80">
-    <section> 
-        <b style="font-size:25px; color:navy">Student 3</b>
-        <ul>
-        <li style="font-size:20px">Name:</li>
-        <li style="font-size:20px">Geburtsdatum:</li>
-        <li style="font-size:20px">Geschlecht:</li>
-        <li style="font-size:20px">Studiengang:</li>
-        <li style="font-size:20px">Matrikelnummer:</li>
-        </ul>
-    </section>
-</section>
-<section class="ItemCard">
-    <img width="200px"src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80">
-    <section> 
-        <b style="font-size:25px; color:navy">Student 4</b>
-        <ul>
-        <li style="font-size:20px">Name:</li>
-        <li style="font-size:20px">Geburtsdatum:</li>
-        <li style="font-size:20px">Geschlecht:</li>
-        <li style="font-size:20px">Studiengang:</li>
-        <li style="font-size:20px">Matrikelnummer:</li>
-        </ul>
-    </section>
-</section>
-<section class="ItemCard">
-    <img width="200px"src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80">
-    <section> 
-        <b style="font-size:25px; color:navy">Student 5</b>
-        <ul>
-        <li style="font-size:20px">Name:</li>
-        <li style="font-size:20px">Geburtsdatum:</li>
-        <li style="font-size:20px">Geschlecht:</li>
-        <li style="font-size:20px">Studiengang:</li>
-        <li style="font-size:20px">Matrikelnummer:</li>
-        </ul>
-    </section>
-</section>
-<section class="ItemCard">
-    <img width="200px"src="https://images.unsplash.com/photo-1484517186945-df8151a1a871?ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80">
-    <section> 
-        <b style="font-size:25px; color:navy">Student 6</b>
         <ul>
         <li style="font-size:20px">Name:</li>
         <li style="font-size:20px">Geburtsdatum:</li>
