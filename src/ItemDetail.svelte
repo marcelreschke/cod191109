b7b556c64d0f8671b232bf8abf6218cfbaf7b367
Marcel Reschke <hello@marcelreschke.com>
2019-11-14 14:02:05 +0100

    Add ItemDetail-Component

---
+++ src/ItemDetail.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<style>
+
+.itemDetail {
+margin: 50px 500px;
+background-color: rgb(202, 202, 202)
+}
+
+</style>
+
+
+<div class="itemDetail" align='center'>
+    <img  src="https://images.unsplash.com/photo-1489481039754-8701aeda983b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80" alt="persona" align="center" width="100%" height="100%">
+    <h1 style='text-align:center;'>Hier ist der Titel</h1>
+    <p style='text-align:center;'>Hier steht ein Text mit vielen Infos die sehr wichtig sind und jeder will sie wissen</p>
+
+    <ui align='left'>
+        <li>1</li>
+         <li>2</li>
+          <li>3</li>
+    </ui>
+
+<iframe width="420" height="315"
+src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1" align="center">
+</iframe>
+
+</div>
+
