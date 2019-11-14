0285b169e49d43188a209bf492bbd71232905210
Marcel Reschke <hello@marcelreschke.com>
2019-11-14 14:22:52 +0100

    Add Header to App

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
     import Nested from './Nested.svelte';
     import Button from './Button.svelte';
+    import Header from './Header.svelte';
+    import ItemDetail from './ItemDetail.svelte';
+    import ItemList from './ItemList.svelte';
+    import Modal from './Modal.svelte';
+    import './studentendaten.json';
    
    function handleMessage(event) {
         alert(event.detail.text);
@@ -10,7 +15,7 @@
 <style>
 
 </style>
-
+<Header title="Studentendaten" subtitle="Deine Studentendaten auf einen Blick"/>
 <h1><center>Time for a project!</center></h1>
 <Nested/>
 <Button ButtonText="Start" AltButtonText="Finish" on:message={handleMessage}/>
