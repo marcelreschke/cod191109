54f9783e9d1da95403cf6bdb1cae8f20594166c8
Marcel Reschke <hello@marcelreschke.com>
2019-11-14 13:11:35 +0100

    Add ItemList-Component

---
+++ src/ItemList.svelte
@@ -0,0 +1,81 @@
+<style>
+@import url('https://fonts.googleapis.com/css?family=Dosis&display=swap');
+h1,p {font-family:Dosis;}
+
+h1 {
+    font-weight: 700;
+    line-height: 1.5;
+}
+
+p {
+    line-height: 0.8;
+
+}
+
+.student { 
+    background-color: lightskyblue;
+    width: 400px;
+    padding: 10px;
+    border: solid 4px #000000;
+    margin:10px;
+
+
+}
+
+.liste { 
+    background-color: rgb(68, 141, 215);
+    padding: 10px;
+    border: solid 4px #000000;
+    margin:10px;
+
+}
+</style>
+
+<section class="liste">
+    <section class="student">
+        <h1>Student 1</h1>
+        <p class="name"><u>Name: </u> Franka Stroetzel </p>
+        <p><u>Geburtsdatum: </u> 10.02.1998 </p>
+        <p><u>Geschlecht: </u> Weiblich </p>
+        <p><u>Studiengang: </u> CMM </p>
+        <p><u>Martrikelnummer: </u> 47304793 </p>
+        <p><u>Foto: </u></p>
+    </section>
+    <section class="student">
+        <i><b>Student 2</b></i>
+        <p><u>Name: </u></p>
+        <p><u>Geburtsdatum: </u> </p>
+        <p><u>Geschlecht: </u></p>
+        <p><u>Studiengang: </u> <p>
+        <p><u>Martrikelnummer: </u></p>
+        <p><u>Foto: </u></p>
+    </section>
+    <section class="student">
+        <i><b>Student 3</b></i>
+        <p><u>Name: </u></p>
+        <p><u>Geburtsdatum: </u> </p>
+        <p><u>Geschlecht: </u></p>
+        <p><u>Studiengang: </u><p>
+        <p><u>Martrikelnummer: </u></p>
+        <p><u>Foto: </u></p>
+    </section>
+    <section class="student">
+        <i><b>Student 4</b></i>
+        <p><u>Name: </u></p>
+        <p><u>Geburtsdatum: </u> </p>
+        <p><u>Geschlecht: </u></p>
+        p><u>Studiengang: </u><p>
+        <p><u>Martrikelnummer: </u></p>
+        <p><u>Foto: </u></p>
+    </section>
+    <section class="student">
+        <i><b>Student 5</b></i>
+        <p><u>Name: </u></p>
+        <p><u>Geburtsdatum: </u></p>
+        <p><u>Geschlecht: </u></p>
+        <p><u>Studiengang: </u><p>
+        <p><u>Martrikelnummer: </u></p>
+        <p><u>Foto: </u></p>
+    </section>
+
+</section>
