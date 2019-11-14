c3e3a57b1731d40ee343ec0db7bbe76c43015db1
Marcel Reschke <hello@marcelreschke.com>
2019-11-14 13:00:32 +0100

    Add Modal-Component

---
+++ src/Modal.svelte
@@ -0,0 +1,54 @@
+<style>
+
+@import url('https://fonts.googleapis.com/css?family=Playfair+Display&display=swap');
+
+h1 {
+    font-size: 32px;
+    color: maroon;
+    font-family: Playfair Display; 
+}
+
+p {font-size: 20px;
+    color: grey;
+    font-family: Playfair Display;     
+}
+
+.hintergrund {
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(211, 211, 211, .4);
+    position: absolute;
+
+
+}
+.modalinhalt {
+    margin-left: auto;
+    margin-right: auto; 
+    width: 600px;
+    background-color: #ffffff;
+    padding: 20px;
+    margin-top: 40px; 
+
+}
+
+
+
+</style>
+
+<div class="hintergrund">
+<div class="modalinhalt">
+
+
+<h1> 
+Vorname Nachname 
+</h1>
+<p>Bild </p>
+<p>Geburtsdatum </p>
+<p>Geschlecht</p>
+<p>Immatrikulationsnummer</p>
+<p>Studiengang</p>
+</div>
+
+<a href="">Schließen</a>
+</div>
+
